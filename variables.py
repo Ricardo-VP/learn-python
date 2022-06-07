52b229a35b59d65258505c2982275a3ad162e521
ricardo-vaca <ricardo.vaca@ioet.com>
2022-06-07 12:32:38 -0500

    Variables 🍿

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Case sensitive
+book = "Book 1"
+Book = "Book 1"
+print(book, Book)
+
+# Multiple definitions
+x, y = 1, 2
+print(x, y)
+
+# Conventions
+book_name = "First robot" # Snake case (popular)
+bookName = "Second robot" # Camel case
+BookName = "Third robot" # Pascal case
+MY_NAME = "Ricardo" # Constant
+print(MY_NAME)
