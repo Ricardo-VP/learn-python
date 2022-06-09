e60e849699e3edba1b06d677d95e58a705be1527
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-09 00:52:08 -0500

    Conditionals 🏐

---
+++ conditionals.py
@@ -0,0 +1,40 @@
+x = 21
+
+if x < 20:
+    print('x is less than 20')
+else:
+    print('x is higher than 20')
+
+#
+
+color = 'blue'
+
+if color == 'red':
+    print('The color is red')
+elif color == 'blue':
+    print('The color is blue')
+else:
+    print('any color')
+
+#
+
+name = 'Ricardo'
+last_name = 'Vaca'
+
+if name == 'Ricardo':
+    if last_name == 'Vaca':
+        print('You are Ricardo Vaca')
+    else:
+        print('You are not Ricardo Vaca')
+else:
+    print('You are not Ricardo Vaca')
+
+#
+
+a = 5
+b = 6
+
+if a > 2 and x <= 10:
+    print('a is greater than 2 and less or equal to ten')
+if a != b:
+    print('a is different than b')
