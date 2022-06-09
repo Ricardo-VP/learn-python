da96b5f4036f400fc408f8efd9fd5fbd6fac687c
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-09 00:58:49 -0500

    Loops 🥞

---
+++ loops.py
@@ -0,0 +1,23 @@
+# For loop
+
+meals = ['apples', 'bread', 'cheese', 'milk']
+
+for meal in meals:
+    if meal == 'milk':
+        print('You have to buy this! ' + meal)
+    print(meal)
+
+for number in range(1, 8):
+    print(number)
+
+for letter in 'Hello':
+    print(letter)
+
+
+# While loop
+
+count = 4
+
+while count <= 10:
+    print(count)
+    count += 1
