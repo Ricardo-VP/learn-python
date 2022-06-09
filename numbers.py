76204c345f18f6444bf650cfeae74c1a099c9375
ricardo-vaca <ricardo.vaca@ioet.com>
2022-06-08 21:28:25 -0500

    Numbers ✨

---
+++ numbers.py
@@ -0,0 +1,12 @@
+# Operations
+# print(3 + 2)   # Addition
+# print(3 - 2)   # Subtraction
+# print(3 * 2)   # Multiplication
+# print(3 / 2 )  # Division
+# print(2 ** 3)  # Elevation
+# print(10 // 2) # Module (5)
+# print(10 % 2)  # Remainder (0)
+
+age = input('Insert your age: ')
+new_age = int(age) + 5
+print(new_age)
