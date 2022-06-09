5801f2b7f145c561611dbd339711def47c0f5de4
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-08 22:30:24 -0500

    Tuples

---
+++ tuples.py
@@ -0,0 +1,18 @@
+x = (1,) # not (1)
+# print(x) 
+# (1)
+
+y = tuple((1, 2, 3, 4, 5))
+# print(y)
+
+# Tuple methods (All the unmutable lists methods)
+# print(dir(y))
+
+# 1. X Element
+print(y[0]) 
+# 1
+
+locations = {
+    (35.15123, 39.12323): "Tokyo",
+    (36.234124, 52.123213): "New York"
+}
